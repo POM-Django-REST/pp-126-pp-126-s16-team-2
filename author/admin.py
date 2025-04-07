7fcdb3591b64b6622d25a8a2fa39a254bb39319a
Tetiana <tmend17@gmail.com>
2025-04-07 11:27:04 +0300

    Додано .gitignore для ігнорування непотрібних файлів та очищено кеш

--- author/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Author
-
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'surname', 'patronymic')  # Відображення імені, прізвища та по-батькові
-    search_fields = ('name', 'surname')  # Пошук за іменем та прізвищем
-    list_filter = ('surname',)  # Фільтр за прізвищем
-
-admin.site.register(Author, AuthorAdmin)
